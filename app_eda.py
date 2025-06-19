72484c264481cdc8875970e1cea03b0b7e8578d5
Yu-Chan-Jeong <thor49337@gmail.com>
2025-06-19 02:33:59 +0000

    11

--- app_eda.py
+++ app_eda.py
@@ -219,13 +219,12 @@ class EDA:
 
         # 4) Pivot on Korean region names, then rename columns to English
         region_map = {
-            '서울특별시':'Seoul','부산광역시':'Busan','대구광역시':'Daegu',
-            '인천광역시':'Incheon','광주광역시':'Gwangju','대전광역시':'Daejeon',
-            '울산광역시':'Ulsan','세종특별자치시':'Sejong','세종':'Sejong',
-            '경기도':'Gyeonggi','강원도':'Gangwon','충청북도':'Chungbuk',
-            '충청남도':'Chungnam','전라북도':'Jeonbuk','전라남도':'Jeonnam',
-            '경상북도':'Gyeongbuk','경상남도':'Gyeongnam','제주특별자치도':'Jeju',
-            '제주도':'Jeju'
+            '서울':'Seoul','부산':'Busan','대구':'Daegu',
+            '인천':'Incheon','광주':'Gwangju','대전':'Daejeon',
+            '울산':'Ulsan','세종':'Sejong',
+            '경기':'Gyeonggi','강원':'Gangwon','충청':'Chungbuk',
+            '충청':'Chungnam','전':'Jeonbuk','전남':'Jeonnam',
+            '경북':'Gyeongbuk','경남':'Gyeongnam','제주':'Jeju',
         }
         # Pivot
         pivot_raw = region_df.pivot_table(
