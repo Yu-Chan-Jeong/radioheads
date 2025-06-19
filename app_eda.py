c81c44972d861f0b7c07d021dca00490ca0ac13d
Yu-Chan-Jeong <thor49337@gmail.com>
2025-06-19 02:35:17 +0000

    11

--- app_eda.py
+++ app_eda.py
@@ -222,8 +222,8 @@ class EDA:
             '서울':'Seoul','부산':'Busan','대구':'Daegu',
             '인천':'Incheon','광주':'Gwangju','대전':'Daejeon',
             '울산':'Ulsan','세종':'Sejong',
-            '경기':'Gyeonggi','강원':'Gangwon','충청':'Chungbuk',
-            '충청':'Chungnam','전':'Jeonbuk','전남':'Jeonnam',
+            '경기':'Gyeonggi','강원':'Gangwon','충북':'Chungbuk',
+            '충남':'Chungnam','전북':'Jeonbuk','전남':'Jeonnam',
             '경북':'Gyeongbuk','경남':'Gyeongnam','제주':'Jeju',
         }
         # Pivot
